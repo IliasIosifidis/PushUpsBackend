e033133b3255b40e45a98d59ea87a27037527e09
ilias iosifidis <iosifidis90@gmail.com>
2026-03-17 21:44:07 +0200

    update gitignore

--- src/main/kotlin/org/pushups/gymgoers/config/SecurityConfig.kt
+++ src/main/kotlin/org/pushups/gymgoers/config/SecurityConfig.kt
@@ -1,4 +1,28 @@
 package org.pushups.gymgoers.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
 class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors {  }
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/api/member/**").hasRole("ADMIN")
+                    .requestMatchers("/api/booking/weekly").authenticated()
+                    .requestMatchers("/api/booking/**").authenticated()
+                    .anyRequest().permitAll()
+            }
+            .oauth2Login {  }
+        return http.build()
+    }
 }
